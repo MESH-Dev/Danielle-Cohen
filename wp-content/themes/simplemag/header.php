9d6cc26ca8644e3ac470af0d4d92d9c5e0cc48b4
robbiesh <sherrard.rob@gmail.com>
2015-02-25 15:42:03 -0500

    first commit

---
+++ wp-content/themes/simplemag/header.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * The Header for the theme
+ *
+ * @package SimpleMag
+ * @since 	SimpleMag 1.0
+**/
+?>
+<!DOCTYPE html>
+<!--[if lt IE 9]><html <?php language_attributes(); ?> class="oldie"><![endif]-->
+<!--[if (gte IE 9) | !(IE)]><!--><html <?php language_attributes(); ?> class="modern"><!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo('charset'); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<?php global $ti_option; ?>
+<link rel="shortcut icon" href="<?php echo $ti_option['site_favicon']['url']; ?>" />
+<link rel="apple-touch-icon-precomposed" href="<?php echo $ti_option['site_retina_favicon']['url']; ?>" />
+
+<?php wp_head(); ?>
+
+<script src="//use.typekit.net/ehy7eux.js"></script>
+<script>try{Typekit.load();}catch(e){}</script>
+
+<link href='http://fonts.googleapis.com/css?family=Noticia+Text:400,400italic,700italic' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
+
+    <div id="pageslide" class="st-menu st-effect">
+    	<a href="#" id="close-pageslide"><i class="icomoon-remove-sign"></i></a>
+    </div><!-- Sidebar in Mobile View -->
+
+	<?php
+    // Check for a layout options: Full Width or  Boxed
+    if ( $ti_option['site_layout'] == '2' ) { $site_layout = ' class="layout-boxed"'; } else { $site_layout = ' class="layout-full"'; } ?>
+    <section id="site"<?php echo isset( $site_layout ) ? $site_layout : ''; ?>>
+        <div class="site-content">
+
+            <header id="masthead" role="banner" class="clearfix<?php if ( $ti_option['site_main_menu'] == true ) { echo ' with-menu'; } ti_top_strip_class(); ?>" itemscope itemtype="http://schema.org/WPHeader">
+
+                <div class="no-print top-strip">
+                    <div class="wrapper clearfix">
+
+                        <?php
+                        // Hide Search and Social Icons if header variation with search is selected
+                        if ( $ti_option['site_header'] != 'header_search' ) {
+
+                            // Search Form
+                            get_search_form();
+
+                            // Social Profiles
+                            if( $ti_option['top_social_profiles'] == 1 ) {
+                                get_template_part ( 'inc/social', 'profiles' );
+                            }
+                        }
+                        ?>
+
+                        <a href="#" id="open-pageslide" data-effect="st-effect"><i class="icomoon-menu"></i></a>
+
+                        <?php
+                        // Pages Menu
+                        if ( has_nav_menu( 'secondary_menu' ) ) :
+                            echo '<nav class="secondary-menu" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">';
+                            wp_nav_menu( array(
+                                'theme_location' => 'secondary_menu',
+                                'container' => false,
+                            ));
+                           echo '</nav>';
+                         endif;
+                        ?>
+                    </div><!-- .wrapper -->
+                </div><!-- .top-strip -->
+
+                <div id="header-container">
+
+                  <div id="branding" class="animated">
+                      <div class="wrapper">
+                      <?php
+                          /**
+                           *  Header Variations
+                          **/
+
+                          // Logo, Social Icons and Search
+                          if ( $ti_option['site_header'] == 'header_search' ) {
+                              get_template_part( 'inc/header', 'search' );
+
+                          // Logo and Ad unit
+                          } elseif ( $ti_option['site_header'] == 'header_banner' ) {
+                              get_template_part( 'inc/header', 'banner' );
+
+                          // Default - Centered Logo and Tagline
+                          } else {
+                              get_template_part( 'inc/header', 'default' );
+                          }
+                      ?>
+                      </div><!-- .wrapper -->
+                  </div><!-- #branding -->
+
+  				<?php
+                  // Main Menu
+                  if ( $ti_option['site_main_menu'] == true ):
+  					if ( has_nav_menu( 'main_menu' ) ) :
+  					echo '<div class="no-print animated main-menu-container">';
+  						if ( $ti_option['site_fixed_menu'] == '3' && $ti_option['site_main_menu'] == true ):
+  							echo '<div class="main-menu-fixed">';
+  						endif;
+  							echo '<nav class="wrapper main-menu" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">';
+  								// wp_nav_menu( array(
+  								// 	'theme_location' => 'main_menu',
+  								// 	'container' => false,
+  								// 	'walker' => new TI_Menu()
+  								//  ));
+
+                  ?>
+
+                  <ul id="menu-main-menu">
+                    <li class="menu-item menu-item-type-taxonomy menu-item-object-category sub-menu-two-columns link-arrow">
+                      <a href="<?php echo get_permalink(get_page_by_title('Homepage')); ?>"><img src="<?php echo get_template_directory_uri() ?>/images/DaniCohen_icon_home.png" /></a><br/>
+                      <a href="<?php echo get_permalink(get_page_by_title('Homepage')); ?>"><h3>Home</h3></a>
+                    </li>
+                    <li class="menu-item menu-item-type-taxonomy menu-item-object-category sub-menu-two-columns link-arrow">
+                      <a href="<?php echo get_permalink(get_page_by_title('Reviews')); ?>"><img src="<?php echo get_template_directory_uri() ?>/images/DaniCohen_icon_reviews.png" /></a><br/>
+                      <a href="<?php echo get_permalink(get_page_by_title('Reviews')); ?>"><h3>Reviews</h3></a>
+                    </li>
+                    <li class="menu-item menu-item-type-taxonomy menu-item-object-category sub-menu-two-columns link-arrow">
+                      <a href="<?php echo get_permalink(get_page_by_title('Playlists')); ?>"><img src="<?php echo get_template_directory_uri() ?>/images/DaniCohen_icon_playlists.png" /></a><br/>
+                      <a href="<?php echo get_permalink(get_page_by_title('Playlists')); ?>"><h3>Playlists</h3></a>
+                    </li>
+                    <li class="menu-item menu-item-type-taxonomy menu-item-object-category sub-menu-two-columns link-arrow">
+                      <a href="<?php echo get_permalink(get_page_by_title('About')); ?>"><img src="<?php echo get_template_directory_uri() ?>/images/DaniCohen_icon_about.png" /></a><br/>
+                      <a href="<?php echo get_permalink(get_page_by_title('About')); ?>"><h3>About</h3></a>
+                    </li>
+                    <li class="menu-item menu-item-type-taxonomy menu-item-object-category sub-menu-two-columns link-arrow">
+                      <a href="<?php echo get_permalink(get_page_by_title('Contact')); ?>"><img src="<?php echo get_template_directory_uri() ?>/images/DaniCohen_icon_contact.png" /></a><br/>
+                      <a href="<?php echo get_permalink(get_page_by_title('Contact')); ?>"><h3>Contact</h3></a>
+                    </li>
+                  </ul>
+
+                  <?php
+
+  							echo '</nav>';
+  						if ( $ti_option['site_fixed_menu'] == '3' && $ti_option['site_main_menu'] == true ):
+  							echo '</div>';
+  						endif;
+  						echo '</div>';
+  					 else:
+  						echo '<div class="message warning"><i class="icomoon-warning-sign"></i>' . __( 'Define your site main menu', 'themetext' ) . '</div>';
+  					 endif;
+                  endif;
+                  ?>
+
+                </div>
+
+            </header><!-- #masthead -->
---
+++ wp-content/themes/simplemag/index.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * The main template file
+ *
+ * @package SimpleMag
+ * @since   SimpleMag 1.0
+**/
+get_header();
+global $ti_option;
+?>
+
+<?php $archive_sidebar = get_field( 'page_sidebar', get_option('page_for_posts') ); ?>
+
+    <section id="content" role="main" class="clearfix animated">
+
+		<?php if ( $ti_option['posts_page_title'] == 'full_width_title' ) : ?>
+        <header class="entry-header page-header">
+            <div class="wrapper title-with-sep page-title">
+                <h1 class="entry-title">
+                    <?php
+                    $posts_page_id = get_option( 'page_for_posts' );
+                    echo get_the_title( $posts_page_id );
+                    ?>
+                </h1>
+            </div>
+        </header>
+        <?php endif; ?>
+
+        <div class="wrapper">
+		<?php
+        // Enable/Disable sidebar based on the field selection
+        if ( ! $archive_sidebar || $archive_sidebar == 'page_sidebar_on' ):
+        ?>
+            <div class="grids">
+
+              <div class="grid-8 column-1">
+
+              <section class="wrapper home-section featured-posts">
+
+              	<?php if( get_sub_field( 'featured_main_title' ) ): ?>
+                  <header class="section-header">
+                      <div class="title-with-sep">
+                          <h2 class="title"><?php the_sub_field( 'featured_main_title' ); ?></h2>
+                      </div>
+                      <?php if( get_sub_field( 'featured_sub_title' ) ): ?>
+                      <span class="sub-title"><?php the_sub_field( 'featured_sub_title' ); ?></span>
+                      <?php endif; ?>
+                  </header>
+                  <?php endif; ?>
+
+                  <?php
+                  /**
+                   * Add posts to this section only if the 'Make Featured'
+                   * custom field checkbox was checked on the Post edit page
+                  **/
+                  $fposts_to_show = get_sub_field( 'featured_posts_per_page' );
+                  $ti_featured_posts = new WP_Query(
+                      array(
+                          'post_type' => 'post',
+                          'meta_key' => 'featured_post_add',
+                          'meta_value' => '1',
+                          'posts_per_page' => 1
+                      )
+                  );
+                  ?>
+
+                  <div class="grids entries">
+
+              	<?php
+                  if ( $ti_featured_posts->have_posts() ) :
+                      while ( $ti_featured_posts->have_posts() ) : $ti_featured_posts->the_post(); ?>
+
+                          <article <?php post_class("grid-8"); ?>>
+
+                              <figure class="entry-image">
+                                  <a href="<?php the_permalink(); ?>">
+                                      <?php
+                                      if ( has_post_thumbnail() ) {
+                                          the_post_thumbnail( 'rectangle-size' );
+                                      } elseif( first_post_image() ) { // Set the first image from the editor
+                                          echo '<img src="' . first_post_image() . '" class="wp-post-image" alt="' . get_the_title() . '" />';
+                                      } ?>
+                                  </a>
+                              </figure>
+
+                              <header class="entry-header">
+                                  <div class="entry-meta">
+                                     <?php ti_meta_data(); ?>
+                                  </div>
+                                  <h2 class="entry-title">
+                                      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                  </h2>
+                                  <?php
+                                  if( $ti_option['site_author_name'] == 1 ) { ?>
+                                  
+                                  <?php } ?>
+                              </header>
+
+                              <?php if( get_sub_field( 'featured_excerpt' ) == 'enable' ) { ?>
+                              <div class="entry-summary">
+                                  <?php the_excerpt(); ?>
+                              </div>
+                              <?php } ?>
+
+                          </article>
+
+                      <?php endwhile; ?>
+
+                      <?php wp_reset_postdata(); ?>
+
+                  </div>
+
+                  <?php else : ?>
+
+                  <p class="message">
+                      <?php _e( 'There are no featured posts yet', 'themetext' ); ?>
+                  </p>
+
+                  <?php endif; ?>
+
+              </section><!-- Featured Posts -->
+
+
+		<?php endif; ?>
+
+                <?php if ( $ti_option['posts_page_title'] == 'above_content_title' ) : ?>
+                <header class="entry-header page-header">
+                    <div class="title-with-sep page-title">
+                        <h1 class="entry-title">
+							<?php
+                            $posts_page_id = get_option( 'page_for_posts' );
+                            echo get_the_title( $posts_page_id );
+                            ?>
+                        </h1>
+                    </div>
+                </header>
+                <?php endif; ?>
+
+                <div class="grids <?php echo $ti_option['posts_page_layout']; ?> entries">
+					<?php
+                    if ( have_posts() ) : while ( have_posts()) : the_post();
+
+                      if (!get_field('featured_post_add')) {
+                        get_template_part( 'content', 'post' );
+                      }
+
+                    endwhile;
+                    ?>
+                </div>
+
+                <?php ti_pagination(); ?>
+
+                <?php else : ?>
+
+                <p class="message">
+                	<?php _e( 'Sorry, no posts were found', 'themetext' ); ?>
+                </p>
+
+                <?php endif;?>
+
+                <?php
+                // Enable/Disable sidebar based on the field selection
+                if ( ! $archive_sidebar || $archive_sidebar == 'page_sidebar_on' ):
+                ?>
+                </div><!-- .grid-8 -->
+
+                <?php get_sidebar(); ?>
+
+            </div><!-- .grids -->
+    		<?php endif; ?>
+
+        </div><!-- .wrapper -->
+    </section><!-- #content -->
+
+<?php get_footer(); ?>
